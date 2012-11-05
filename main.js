d6329e3c7b33105a499f6e46bc6050d17fdd25dc
Scott McClaugherty <gamefreak4321@gmail.com>
2012-11-05 17:10:43 -0500

    Stuff

--- main.js
+++ main.js
@@ -1,42 +1,15 @@
-function polypoints(arr, i) {
-	var circles = [0.0, 36.0, 59.0, 79.0, 98.0, 144.0, 190.0, 239.0];
-	var x1 = arr[0],
-		y1 = arr[1],
-		x2 = arr[2],
-		y2 = arr[3];
-	var r1 = circles[i]/60,
-		r2 = circles[i+1]/60.0;
-	var d = Math.sqrt((x1-x2)*(x1-x2) + (y1-y2)*(y1-y2));
-	var vx = (x2-x1)/d,
-		vy = (y2-y1)/d;
-	var c = (r1-r2)/d
-	if (c*c > 1.0) {
-		return "";
-	}
-	var h = Math.sqrt(Math.max(0.0, 1.0-c*c));
-
-	var quad_points = [];
-	{
-		var nx = vx * c - (-1) * h * vy;
-		var ny = vy * c + (-1) * h * vx;
-
-		quad_points.push([x1+r1*nx, y1+r1*ny].join(","));
-		quad_points.push([x2+r2*nx, y2+r2*ny].join(","));
-	}
-	{
-		var nx = vx * c - (+1) * h * vy;
-		var ny = vy * c + (+1) * h * vx;
-		
-		//swapped
-		quad_points.push([x2+r2*nx, y2+r2*ny].join(","));
-		quad_points.push([x1+r1*nx, y1+r1*ny].join(","));
-	}
-	
-	return quad_points.join(" ");
-}
 var SCALE = 10;
-
-d3.json("sandy.json", function(data) {
+var jsonFile = "";
+switch (location.hash) {
+	default:
+	case "#sandy":
+		jsonFile = "sandy.json";
+		break;
+	case "#isaac":
+		jsonFile = "isaac.json";
+		break;
+}
+d3.json(jsonFile, function(data) {
 	console.log(data);
 	window.data = data;
 
@@ -57,7 +30,9 @@ d3.json("sandy.json", function(data) {
 
 	var opacityLayer = transformLayer.append("g")
 	opacityLayer.style("opacity", ".5")
-	var scales = data.forecast.map(function(e1, i, a) {
+	var scales = data.forecast
+		// .sort(function(a,b){return d3.descending(a.range, b.range); })
+		.map(function(e1, i, a) {
 		return d3.time.scale()
 		.domain(e1.points.map(function(e2,i,a){
 			return new Date((e2.time-e1.range*3600)*1000);
@@ -68,45 +43,79 @@ d3.json("sandy.json", function(data) {
 
 	window.scales = scales;
 
-	var subLayers = opacityLayer.selectAll("g").data(scales);
+	var subLayers = opacityLayer.selectAll("g").data(scales.map(function(e,i,a){
+		return {interpolator: e, index: i};
+	}));
 	var subLayersEntered = subLayers.enter().append("g")
 		.style("fill", function(dat, i){
 			return d3.rgb(255, 0, 255).darker(((7-i)/3.0))
 		});
-	var circles = subLayersEntered.append("circle")
-	circles.attr("r", function(r,i){
-			return ([0.0, 36.0, 59.0, 79.0, 98.0, 144.0, 190.0, 239.0])[i]/60.0;
-	});
-	circles
+
+	subLayersEntered.append("polygon").property("index", function(r, i) {return i});
+	subLayersEntered.append("circle").attr("r", function(r,i){return data.ranges[i]});
+
+	subTrans = subLayers
 		.transition()
-		.duration(5000)
+		.delay(1000)
+		.duration(15000)
 		.ease("linear")
-		.tween("att", function(interp,i){
+	subTrans.selectAll("circle")
+		.tween("att", function(dat,i){
 			return function(t){
-				var da = new Date((data.times.from + (data.times.to-data.times.from)*t)*1000);
-				v =  interp(da);
-				this.setAttribute("cx", (v[0]));
-				this.setAttribute("cy", (v[1]));
+				var date = new Date((data.times.from + (data.times.to-data.times.from)*t)*1000);
+				pos = dat.interpolator(date);
+				this.setAttribute("cx", pos[0]);
+				this.setAttribute("cy", pos[1]);
 			}
 		});
 
-	var polys = subLayersEntered.append("polygon")
-	polys
-		.transition()
-		.duration(5000)
-		.ease("linear")
-		.tween("att", function(dd, i) {
+	
+	subTrans.selectAll("polygon")
+		.tween("att", function(dat, i) {
 			return function(t) {
-				// console.log(this.select);
-				var da = new Date((data.times.from + (data.times.to-data.times.from)*t)*1000);
-				// var da = (data.times.from + (data.times.to-data.times.from)*t);
-				// v =  dd(da);
-				var v =  dd(da).concat(
-					scales[Math.min(i+1,scales.length-1)
-						](da));
+				var date = new Date((data.times.from + (data.times.to-data.times.from)*t)*1000);
+				// var i = this.index;
+				var v1 = dat.interpolator(date);
+				var v2 = scales[Math.min(dat.index+1,scales.length-1)](date);
 				// if (i==7)console.log(v, polypoints(v, i));
 				if (i != 7)
-				this.setAttribute("points", polypoints(v, i));
+				this.setAttribute("points", polypoints(v1, v2, dat.index));
 		};
 	});
+
+	function polypoints(p1, p2, i) {
+		var x1 = p1[0],
+			y1 = p1[1],
+			x2 = p2[0],
+			y2 = p2[1];
+		var r1 = data.ranges[i],
+			r2 = data.ranges[i+1];
+		var d = Math.sqrt((x1-x2)*(x1-x2) + (y1-y2)*(y1-y2));
+		var vx = (x2-x1)/d,
+			vy = (y2-y1)/d;
+		var c = (r1-r2)/d
+		if (c*c > 1.0) {
+			return "";
+		}
+		var h = Math.sqrt(Math.max(0.0, 1.0-c*c));
+
+		var quad_points = [];
+		{
+			var nx = vx * c - (-1) * h * vy;
+			var ny = vy * c + (-1) * h * vx;
+
+			quad_points.push([x1+r1*nx, y1+r1*ny].join(","));
+			quad_points.push([x2+r2*nx, y2+r2*ny].join(","));
+		}
+		{
+			var nx = vx * c - (+1) * h * vy;
+			var ny = vy * c + (+1) * h * vx;
+			
+			//swapped
+			quad_points.push([x2+r2*nx, y2+r2*ny].join(","));
+			quad_points.push([x1+r1*nx, y1+r1*ny].join(","));
+		}
+		
+		return quad_points.join(" ");
+	}
 });
