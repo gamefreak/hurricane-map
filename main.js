b00c5c095655cba11d47bb3f69a54a41a1b89a0a
Scott McClaugherty <gamefreak4321@gmail.com>
2012-11-06 15:40:07 -0500

    A bunch more stuff.
    Changed the storm picker to use radio buttons.
    Data no longer includes bounding box or range table.
    Range tables and bounding boxes are calculated on load
    Reversed the order of the shapes so that shorter range predictions appear on top.
    Added more storms.

--- main.js
+++ main.js
@@ -1,8 +1,17 @@
 var SCALE = 15;
-
+var RANGE_TABLE = [
+	[0, 0],
+	[12, 36],
+	[24, 59],
+	[36, 79],
+	[48, 98],
+	[72, 144],
+	[96, 190],
+	[120, 239]
+];
 dataFiles = {}
 // PHILIPPE,bret,irene,katia,lee,maria,ophelia,rhina,sean
-hurricaneList = ["philippe", "sandy", "isaac", "nadine", "beryl", "bret"]
+hurricaneList = ["jova", "philippe", "sandy", "isaac", "nadine", "beryl", "bret", "katia"]
 function load() {
 	var picker = document.getElementById("picker");
 	var hash = location.hash;
@@ -14,7 +23,7 @@ function load() {
 		}
 	}
 	var jsonFile = choice + ".json";
-	picker.value = "#" + choice;
+	document.querySelector('input[name="choice"][value="#'+choice+'"]').checked = true
 	if (jsonFile in dataFiles) newData(dataFiles[jsonFile]);
 	else d3.json(jsonFile, function(json) {
 		dataFiles[jsonFile] = json;
@@ -24,30 +33,32 @@ function load() {
 
 
 document.addEventListener("DOMContentLoaded", function() {
+	var rangeEntries = d3.transpose(RANGE_TABLE);
+	var ranges = d3.scale.linear().domain(rangeEntries[0]).range(rangeEntries[1]);
+	// window.ranges = ranges;
+
 	var options = d3.select("#picker")
-		.selectAll("option")
+		.style("float", "right")
+		.selectAll("span")
 		.data(hurricaneList)
-	options
-		.enter()
-		.append("option")
+	var spans = options
+		.enter().append("span")
+	spans
+		.append("input")
+		.attr("name", "choice")
+		.attr("type", "radio")
+		.attr("id", function(d) {return "strm"+d})
 		.attr("value", function(d,i) {return "#"+d})
+		.on("change", function(data) {
+			location.hash = "#" + data;
+		})
+	spans.append("label")
+		.attr("for", function(d) {return "strm"+d})
 		.text(function(d, i){return d[0].toUpperCase() + d.slice(1).toLowerCase()})
+	spans.append("br")
 	window.addEventListener("hashchange", load);
-	document.getElementById("picker").addEventListener("change", function(){
-		// console.log('a');
-		location.hash = document.getElementById("picker").value;
-	});
 
-	if (location.hash == "") {
-		// document.getElementById("picker").value = "#sandy"
-load();
-	} else {
-		load();
-	}
-// document.getElementById("picker").addEventListener
-
-
-
+	load();
 
 	window.svg = d3.select("body").append("svg")
 		.attr("width", 360)
@@ -77,40 +88,77 @@ load();
 	window.newData = function newData(data) {
 		console.log(data);
 		window.data = data;
+		var bounds;
+		// var left, right, top, bottom;
+		for (var a = 0; a < data.forecast.length; a++) {
+			for (var b = 0; b < data.forecast[a].points.length; b++) {
+				var pt = data.forecast[a].points[b].point;
+
+				var range = ranges(data.forecast[a].range)/60.0;
+
+				if (bounds == null) {
+					bounds = {
+						left: pt.x-range,
+						right: pt.x+range,
+						top: pt.y+range,
+						bottom: pt.y - range
+					};
+				} else {
+					bounds.left = Math.min(bounds.left, pt.x-range);
+					bounds.right = Math.max(bounds.right, pt.x+range);
+					bounds.top = Math.max(bounds.top, pt.y+range);
+					bounds.bottom = Math.min(bounds.bottom, pt.y-range);
+				}
+			}
+		}
+
+		if (data.basin == "EP" && bounds.left > bounds.right) {
+			bounds.left -= 360.0;
+		}
+		// console.log(bounds);
 
 		var scales = data.forecast
 			.map(function(e1, i, a) {
 				return d3.time.scale()
 					.domain(e1.points.map(function(e2,i,a){return new Date((e2.time));}))
-					// .range(e1.points.map(function(e2,i,a){return [e2.point.x, e2.point.y];}))
-					.range(e1.points.map(function(e2,i,a){return e2.point}))
+					.range(e1.points.map(function(e2,i,a){return [e2.point.x, e2.point.y]}))
 					.clamp(true);
 		});
-		var mscales = scales.map(function(int,i,a){
-			return {interpolator: int, interpolator2:scales[Math.min(scales.length-1,i+1)], index: i};
-		});
+		
+		window.scales = scales;
+		var mappedScale = d3.scale.ordinal()
+			.domain(d3.range(8))
+			.range(scales);
+		window.mappedScale = mappedScale;
 
+		var sizes = d3.scale.linear()
+			.domain(d3.range(8))
+			.range(data.forecast.map(function(e,i,a){return e.range;}))
 		svg
-			.attr("width", (data.bounds.xmax-data.bounds.xmin)*SCALE)
-			.attr("height", (data.bounds.ymax-data.bounds.ymin)*SCALE)
+			.attr("width", (bounds.right-bounds.left)*SCALE)
+			.attr("height", (bounds.top-bounds.bottom)*SCALE)
 		transformLayer
-			.attr("transform",  "scale(1, -1) scale("+SCALE+") translate(0 0) translate("+(-data.bounds.xmin)+" "+(-data.bounds.ymax)+") scale(1)")
+			.attr("transform",  "scale(1, -1) scale("+SCALE+") translate(0 0) translate("+(-bounds.left)+" "+(-bounds.top)+") scale(1)")
 
 		window.scales = scales;
 
-		var subLayers = opacityLayer.selectAll("g").data(mscales);
+		var subLayers = opacityLayer.selectAll("g").data(d3.range(8)
+			.reverse()
+			);
 
 		var subLayersEntered = subLayers.enter().append("g")
 			.style("fill", function(dat, i){
-				return d3.rgb(0, 0, 255).darker(((7-i)/3.0))
+				return d3.rgb(0, 0, 255).darker(((8-dat)/3.0))
 			});
 
 		subLayersEntered.append("polygon")
-		subLayersEntered.append("circle").attr("r", function(r,i){return data.ranges[i]});
+		subLayersEntered.append("circle").attr("r", function(r,i){
+			return ranges(sizes(r))/60.0;
+		});
+		// subLayersEntered.append("polygon")
 
 		subLayers.exit().remove();
 
-		// subTransitions = subLayers
 		subTransitions = svg
 			.transition()
 			.delay(500)
@@ -127,11 +175,10 @@ load();
 
 		subTransitions.selectAll("circle")
 			.tween("circle pos", function(dat,i){
-				// console.log(arguments);
 				return function(t){
 					var date = new Date((data.times.from + (data.times.to-data.times.from)*t)*1000);
 					
-					pos = scales[dat.index](date);
+					var pos = mappedScale(dat)(date)
 					this.setAttribute("cx", pos[0]);
 					this.setAttribute("cy", pos[1]);
 				}
@@ -142,11 +189,11 @@ load();
 			.tween("poly verts", function(dat, i) {
 				return function(t) {
 					var date = new Date((data.times.from + (data.times.to-data.times.from)*t)*1000);
-					// var v1 = dat.interpolator(date);
-					var v1 = scales[dat.index](date);
-					var v2 = scales[Math.min(dat.index+1,scales.length-1)](date);
-					// var v2 = dat.interpolator2(date);
-					if (i != 7) this.setAttribute("points", polypoints(v1, v2, dat.index));
+
+					var v1 = mappedScale(dat)(date)
+					var v2 = mappedScale(dat-1)(date)
+					if (dat != 0) this.setAttribute("points", polypoints(v1, v2, dat));
+					this.setAttribute("n", dat)
 			};
 		});
 
@@ -155,8 +202,9 @@ load();
 				y1 = p1[1],
 				x2 = p2[0],
 				y2 = p2[1];
-			var r1 = data.ranges[i],
-				r2 = data.ranges[i+1];
+			var r1 = ranges(sizes(i))/60,
+				r2 = ranges(sizes(i-1))/60;
+
 			var d = Math.sqrt((x1-x2)*(x1-x2) + (y1-y2)*(y1-y2));
 			if (d == 0) return "";
 			var vx = (x2-x1)/d,
@@ -187,9 +235,4 @@ load();
 			return quad_points.join(" ");
 		}
 	};
-
-	// d3.json(jsonFile, function(data){newData(data)});
-
-	// d3.json('sandy2.json', function(d){window.sandy = d})
-	// d3.json('isaac2.json', function(d){window.isaac = d})
 });
