3ea5be8ecadb10699c5faee95d889a2f26fa65eb
Scott McClaugherty <gamefreak4321@gmail.com>
2012-11-14 23:14:43 -0500

    Restructure to display multiple simultaneous storms.

--- main.js
+++ main.js
@@ -15,34 +15,42 @@ var hurricaneList = ["sandy", "jova", "philippe", "irene", "isaac", "nadine", "b
 
 function load() {
 	var picker = document.getElementById("picker");
-	var hash = location.hash;
-	var choice = hurricaneList[0];
+	var hashes = location.hash.length<2?["sandy"]:location.hash.slice(1).split(",");
 	for (var i = 0; i < hurricaneList.length; i++) {
-		if ("#" + hurricaneList[i] == hash) {
-			choice = hurricaneList[i];
-			break;
+		var opt = document.querySelector('option[value="'+hurricaneList[i]+'"]');
+		opt.selected = (hashes.indexOf(hurricaneList[i])!=-1);
+	}
+	loadData(hashes)
+}
+
+function loadData(files) {
+	var data = [];
+	for (var i = 0; i < files.length; i++) {
+		if (files[i] in dataFiles) {
+			data.push(dataFiles[files[i]]);
+		} else {
+			// var jsonFile = "storms/" + choice + ".json";
+			d3.json("storms/" + files[i] + ".json", function(json) {
+				dataFiles[files[i]] = json;
+				loadData(files);
+			});
+			return;
 		}
 	}
-	var jsonFile = "storms/" + choice + ".json";
-	document.querySelector('input[name="choice"][value="#'+choice+'"]').checked = true
-	if (jsonFile in dataFiles) newData(dataFiles[jsonFile]);
-	else d3.json(jsonFile, function(json) {
-		dataFiles[jsonFile] = json;
-		newData(json);
-	});
+	newData(data);
 }
 
 function updateColors(selection) {
 	var col = d3.hsl(document.querySelector("#conecolor").value);
 	selection.style("fill", function(dat, i){
-		return col.brighter(1.1).darker(((7-dat)/3.0));
+		return col.brighter(1.1).darker(((i)/2.0));
 	});
 }
 
 function updateOpacity(selection, opacity) {
-	// console.log(arguments);
-	// var opacity = document.querySelector("#opacityfield").value;
-	// d3.selectAll(".opacity").style("opacity", opacity);
+	if (opacity == null) {
+		opacity = document.querySelector("#opacityfield").value;
+	}
 	selection.style("opacity", opacity);
 }
 
@@ -53,12 +61,12 @@ function zoomUpdate(translate, scale, svg) {
 	} else {
 		translate = [-scale*translate[0], scale*translate[1]];
 		svg.call(d3.behavior.zoom()
-		.x(d3.scale.linear().domain([-180, 180]).range([-180, 180]))
-		.y(d3.scale.linear().domain([90, -90]).range([180, 0]))
-		.translate(translate)
-		.scale(scale)
-		.scaleExtent([2.5, 25.0])
-		.on("zoom", zoomUpdate));
+			.x(d3.scale.linear().domain([-180, 180]).range([-180, 180]))
+			.y(d3.scale.linear().domain([90, -90]).range([180, 0]))
+			.translate(translate)
+			.scale(scale)
+			.scaleExtent([2.5, 25.0])
+			.on("zoom", zoomUpdate));
 	}
 	d3.select("#transform").attr("transform", "translate("+translate+") scale("+scale+") scale(1 -1)");
 }
@@ -76,25 +84,39 @@ document.addEventListener("DOMContentLoaded", function() {
 		d3.selectAll('.opacity').call(updateOpacity, this.value)
 	});
 
-	var options = d3.select("#picker")
-		.selectAll("div")
-		.data(hurricaneList);
 
-	var divs = options
-		.enter().append("div");
+	var options = d3.select("#stormlist")
+		.on("change", function() {
+			console.log(this.value);
+			// console.dir(this);
+			location.hash = "#" + Array.prototype.slice.call(this.selectedOptions).map(function(d){return d.value});
+		})
+		.selectAll("option")
+		.data(hurricaneList)
 
-	divs.append("input")
-		.attr("name", "choice")
-		.attr("type", "radio")
-		.attr("id", function(d) {return "strm"+d})
-		.attr("value", function(d,i) {return "#"+d})
-		.on("change", function(data) {
-			location.hash = "#" + data;
-		});
+	options.enter()
+		.append("option")
+		.attr("value", function(d,i) {return d})
+		.text(function(d, i){return d[0].toUpperCase() + d.slice(1).toLowerCase()})
+	// var options = d3.select("#picker")
+	// 	.selectAll("div")
+	// 	.data(hurricaneList);
 
-	divs.append("label")
-		.attr("for", function(d) {return "strm"+d})
-		.text(function(d, i){return d[0].toUpperCase() + d.slice(1).toLowerCase()});
+	// var divs = options
+	// 	.enter().append("div");
+
+	// divs.append("input")
+	// 	.attr("name", "choice")
+	// 	.attr("type", "radio")
+	// 	.attr("id", function(d) {return "strm"+d})
+	// 	.attr("value", function(d,i) {return "#"+d})
+	// 	.on("change", function(data) {
+	// 		location.hash = "#" + data;
+	// 	});
+
+	// divs.append("label")
+	// 	.attr("for", function(d) {return "strm"+d})
+	// 	.text(function(d, i){return d[0].toUpperCase() + d.slice(1).toLowerCase()});
 
 	window.addEventListener("hashchange", load);
 
@@ -106,169 +128,173 @@ document.addEventListener("DOMContentLoaded", function() {
 
 	var transformLayer = svg.select("g#transform");
 	var stormWrapper = transformLayer.select("#storms");
-	var stormLayer = stormWrapper.append("g").attr("class", "storm")
-	var opacityLayer = stormLayer.append("g").attr("class", "opacity").call(updateOpacity, 0.25)
 
-	var histLayer = stormLayer
-		.append("g")
-		.attr("class", "history");
-	var histLine = histLayer.append("path")
-		.attr("class", "historyline")
-		.style("fill", "none")
-		.style("stroke", "green")
-		.style("stroke-width", "0.1")
 
 	var timestamp = svg.select("#timestamp");
 
 	var slider = svg.select("#slider").style("fill", "grey");
 
-	var rangeEntries = d3.transpose(RANGE_TABLE);
-	var ranges = d3.scale.linear().domain(rangeEntries[0]).range(rangeEntries[1]);
+	var radiusEntries = d3.transpose(RANGE_TABLE);
+	var radii = d3.scale.linear().domain(radiusEntries[0]).range(radiusEntries[1].map(function(d){return d/60.0}));
 
 	window.newData = function newData(data) {
+		// data = data[0];
 		console.log(data);
 		window.data = data;
-		data.forecast.sort(function(a,b) {
-			return d3.ascending(a.range, b.range)
+		data.forEach(function(data, idx, array) {
+			data.forecast.sort(function(a, b) {return d3.descending(a.range, b.range)})
 		});
+
+
 		var bounds = null;
-		// var left, right, top, bottom;
-		for (var a = 0; a < data.forecast.length; a++) {
-			for (var b = 0; b < data.forecast[a].points.length; b++) {
-				var pt = data.forecast[a].points[b].point;
+		for (var s = 0; s < data.length; s++) {
+			for (var a = 0; a < data[s].forecast.length; a++) {
+				for (var b = 0; b < data[s].forecast[a].points.length; b++) {
+					var pt = data[s].forecast[a].points[b].point;
 
-				var range = ranges(data.forecast[a].range)/60.0;
+					var range = radii(data[s].forecast[a].range);
 
-				if (bounds == null) {
-					bounds = {
-						left: pt.x-range,
-						right: pt.x+range,
-						top: pt.y+range,
-						bottom: pt.y - range
-					};
-				} else {
-					bounds.left = Math.min(bounds.left, pt.x-range);
-					bounds.right = Math.max(bounds.right, pt.x+range);
-					bounds.top = Math.max(bounds.top, pt.y+range);
-					bounds.bottom = Math.min(bounds.bottom, pt.y-range);
+					if (bounds == null) {
+						bounds = {
+							left: pt.x-range,
+							right: pt.x+range,
+							top: pt.y+range,
+							bottom: pt.y - range
+						};
+					} else {
+						bounds.left = Math.min(bounds.left, pt.x-range);
+						bounds.right = Math.max(bounds.right, pt.x+range);
+						bounds.top = Math.max(bounds.top, pt.y+range);
+						bounds.bottom = Math.min(bounds.bottom, pt.y-range);
+					}
 				}
 			}
 		}
 		window.bounds = bounds;
+
+
 		if (data.basin == "EP" && bounds.left > bounds.right) {
 			console.log("SHIFT");
 			bounds.left -= 360.0;
 		}
+	
 		console.log(bounds);
-	
-		zoomUpdate([bounds.left,bounds.top], SCALE, svg)
 
-		var shortForcast = data.forecast
-			.filter(function(e, i, a) {return e.points.length >= 2});
-
-		var scales = shortForcast
-			.map(function(e1, i, a) {
-				return d3.time.scale()
-					.domain(e1.points.map(function(e2,i,a){return new Date((e2.time));}))
-					.range(e1.points.map(function(e2,i,a){return [e2.point.x, e2.point.y]}))
-					.clamp(true);
-		});
-	
-		window.scales = scales;
-		var mappedScale = d3.scale.ordinal()
-			.domain(d3.range(scales.length))
-			.range(scales);
-		window.mappedScale = mappedScale;
-
-		var sizes = d3.scale.linear()
-			.domain(d3.range(shortForcast.length))
-			.range(shortForcast.map(function(e,i,a){return e.range;}));
 		svg
 			.attr("width", (bounds.right-bounds.left)*SCALE)
 			.attr("height", (bounds.top-bounds.bottom)*SCALE);
+
+		zoomUpdate([bounds.left,bounds.top], SCALE, svg)
 
 		slider
 			.attr("x", 0)
 			.attr("y", (bounds.top-bounds.bottom)*SCALE-10);
 
-		var subLayers = opacityLayer.selectAll("g").data(d3.range(scales.length).reverse());
 
-		var subLayersEntered = subLayers
-			.enter().append("g").classed('colors', true);
-		subLayers.call(updateColors);
-			
-		// subLayersEntered.append("polygon")
-		subLayersEntered.append("circle")
-			.attr("r", function(r,i){
-				return ranges(sizes(r))/60.0;
-			})
-			.style("stroke", "black")
-			.attr("stroke-width", ".1");
+		var timescale = d3.scale.linear().range([
+			d3.min(data, function(d){return d.times.from})*1000,
+			d3.max(data, function(d){return d.times.to})*1000]);
+		window.timescale = timescale;
 
-		subLayersEntered.append("polygon");
+		var storm = stormWrapper.selectAll("g.storm").data(data);
+		storm.enter()
+			.append("g").classed("storm", true)
+			.call(function(d) {this.append("g").classed("opacity", true).call(updateOpacity)})
+			.call(function(d) {
+				this.append("g").classed("history", true)
+					.append("path").classed("historyline", true);
+			});
+		storm.exit().remove();
 
-		subLayersEntered.append("polyline")
-			.style("stroke", "black")
-			.attr("stroke-width", ".1");
+		var layer = storm.select(".opacity").selectAll("g.colors").data(function(data, i) {
+			return data.forecast.map(function(level, index, array) {
+				var timeDomain = level.points.map(function(point,i,a){return new Date(point.time);});
+				var xRange = level.points.map(function(point,i,a){return point.point.x});
+				var yRange = level.points.map(function(point,i,a){return point.point.y});
+				return {
+					range: level.range,
+					x: d3.time.scale().domain(timeDomain).range(xRange).clamp(true),
+					y: d3.time.scale().domain(timeDomain).range(yRange).clamp(true)
+				};
+			}).map(function(level, index, array) {
+				level.prev = array[Math.min(index + 1, array.length - 1)];
+				return level;
+			});
+		});
+		console.log(layer);
 
-		subLayers.exit().remove();
+		layer.enter().append("g").classed("colors", true)
+			.call(function() {this.append("circle")})
+			.call(function() {this.append("polygon")})
+			.call(function() {this.append("polyline")});
+		layer.exit().remove();
+		layer.call(updateColors);
+		layer.select("circle")
+			.attr("r", function(data, i) {return radii(data.range)})
+			.attr("cx", function(data, i) {return data.x(0)})
+			.attr("cy", function(data, i) {return data.y(0)})
+			.style("stroke", "black").style("stroke-width", "0.1");
+		layer.select("polygon");
+		layer.select("polyline");
 
-		subTransitions = svg
-			.transition()
-			.delay(500)
-			.duration(10000)
-			.ease("linear");
 
 		function masterTween() {
-			var startTime = new Date(data.times.from*1000);
+			// console.log(this, arguments);
+			// var startTime = new Date(data.times.from*1000);
+			var startTime = new Date(timescale(0));
+			var layerdata = layer.data();
+			// console.log(layerdata[layerdata.length-1]);
 			var linePath = d3.svg.line()
 					.interpolate("basis")
-					.x(function(d){return scales[0](d)[0]})
-					.y(function(d){return scales[0](d)[1]});
+					.x(function(d){return layerdata[layerdata.length-1].x(d)})
+					.y(function(d){return layerdata[layerdata.length-1].y(d)});
 
 			var historyLine = svg.select(".historyline");
 
 			var timestamp = svg.select('#timestamp');
 
-			var circles = svg.selectAll("#transform circle")
+			var circle = d3.selectAll("#storms circle")
 			
-			var polys = svg.selectAll("polygon, polyline:not(.historyline)");
+			var poly = svg.selectAll("polygon, polyline:not(.historyline)");
 
 			return function(t) {
-				// console.log(t);
-				var now = new Date((data.times.from + (data.times.to-data.times.from)*t)*1000);
-				
+				// console.log(t, timescale(t), new Date(timescale(t)));
+			// 	var now = new Date((data.times.from + (data.times.to-data.times.from)*t)*1000);
+				var now = new Date(timescale(t));
+
 				var pts = linePath(d3.time.hours(startTime, now, 6)) || "M 0 0";
 				historyLine.attr("d", pts)
 
 				timestamp.property("textContent", now);
-				circles
-					.attr("cx", function(d, i) {return mappedScale(d)(now)[0]})
-					.attr("cy", function(d, i) {return mappedScale(d)(now)[1]})
+				circle
+					.attr("cx", function(d, i) {return d.x(now)})
+					.attr("cy", function(d, i) {return d.y(now)})
 				
-				polys.attr("points", function(d, i) {
-					var v1 = mappedScale(d)(now)
-					var v2 = mappedScale(d-1)(now)
-					return polypoints(v1, v2, d);
+				poly.attr("points", function(data, i) {
+					return polypoints(data.x(now), data.y(now), radii(data.range), data.prev.x(now), data.prev.y(now), radii(data.prev.range));
 				});
 			};
 		}
-		subTransitions.tween("main", masterTween)
 
-		function sliderTween() {
-			return function(t) {
-				var width = (bounds.right-bounds.left)*SCALE-20;
-				slider.attr("x", width*t);
-			};
-		}
-		subTransitions.tween("slider", sliderTween)
-
+			svg
+			.transition()
+			.delay(50)
+			.duration(10000)
+			.ease("linear")
+			.tween("main", masterTween)
+			.tween("slider", function() {
+				return function(t) {
+					var width = (bounds.right-bounds.left)*SCALE-20;
+					slider.attr("x", width*t);
+				}
+			});
 
 
 		var dragger = d3.behavior.drag()
 			.origin(function(d){return {x:parseFloat(this.getAttribute("x")), y:0}})
 			.on("dragstart", function(){svg.transition()})
 			.on("drag", function() {
+				console.log(this);
 				var width = (bounds.right-bounds.left)*SCALE - 20;
 				var x = Math.max(0, Math.min(d3.event.x, width))
 				this.setAttribute("x", x);
@@ -276,14 +302,7 @@ document.addEventListener("DOMContentLoaded", function() {
 			})
 		d3.select("#slider").call(dragger);
 	
-		function polypoints(p1, p2, i) {
-			var x1 = p1[0],
-				y1 = p1[1],
-				x2 = p2[0],
-				y2 = p2[1];
-			var r1 = ranges(sizes(i))/60,
-				r2 = ranges(sizes(i-1))/60;
-
+		function polypoints(x1, y1, r1, x2, y2, r2) {
 			var d = Math.sqrt((x1-x2)*(x1-x2) + (y1-y2)*(y1-y2));
 			if (d == 0) return "";
 			var vx = (x2-x1)/d,
@@ -313,5 +332,5 @@ document.addEventListener("DOMContentLoaded", function() {
 			
 			return quad_points.join(" ");
 		}
-	};
+	}
 });
